ac94df9673bd24aec915587125c51771471abad3
Louis Hsiao <louis.hsiao@d8ai.com>
2020-01-06 21:34:35 +0800

    test: add edit func test

--- src/components/OrderModal.js
+++ src/components/OrderModal.js
@@ -17,9 +17,50 @@ const useStyles = createUseStyles({
   modalContent: {
     backgroundColor: '#fefefe',
     margin: 'auto',
-    padding: 20,
+    padding: '25px 40px',
     border: '1px solid #888',
-    width: '80%',
+    width: '40%',
+  },
+  modalHeader: {
+    display: 'flex',
+  },
+  modalBody: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  modalFooter: {
+    marginTop: 24,
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  inRowClass: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingTop: 16,
+    alignItems: 'flex-start',
+  },
+  textInput: {
+    width: '25vw',
+    marginLeft: 16,
+  },
+  textArea: {
+    marginLeft: 16,
+    width: '25vw',
+    height: 60,
+    resize: 'none',
+  },
+  divier: {
+    flexGrow: 1,
+  },
+  btn: {
+    width: 40,
+    height: 28,
+    lineHeight: '28px',
+    padding: '4px 4px',
+    cursor: 'pointer',
+    border: '#000 1px solid',
+    textAlign: 'center',
+    margin: 4,
   },
   closeBtn: {
     color: '#aaaaaa',
@@ -34,21 +75,62 @@ const useStyles = createUseStyles({
   }
 })
 
+
 const OrderModal = (props) => {
-  const { isEditMode } = props;
+  const { isEditMode, data, onEditOrder } = props;
   const classes = useStyles();
+  const [name, setName] = useState(data.name);
+  const [price, setPrice] = useState(data.price);
+  const [notes, setNotes] = useState(data.notes);
   const [modelVisible, setModelVisible] = useState(false);
   const onClickClose = () => setModelVisible(false);
   const onClickOpen = () => setModelVisible(true);
+  const onClickCancel = onClickClose;
+  const onClickConfirm = () => {
+    onEditOrder({
+      name,
+      price,
+      notes,
+    })
+    onClickClose();
+  }
+  const onChangeName = (e) => setName(e.target.value);
+  const onChangePrice = (e) => setPrice(e.target.value);
+  const onChangeNotes = (e) => setNotes(e.target.value);
   return (
     <div>
-      <button id="myBtn" onClick={onClickOpen}>{isEditMode ? 'Edit' : 'Add'}</button>
+      <button onClick={onClickOpen}>{isEditMode ? 'Edit' : 'Add'}</button>
 
-      {modelVisible && 
-        <div id="myModal" className={classes.modalStyle} onClick={onClickClose}>
-          <div className={classes.modalContent} onClick={(e) => e.stopPropagation()}>
-            <span className={classes.closeBtn} onClick={onClickClose}>&times;</span>
-            <p>Some text in the Modal..</p>
+      {modelVisible && isEditMode &&
+        <div className={classes.modalStyle} onClick={onClickClose}>
+          <div className={classes.modalContent} onClick={(e) => e.stopPropagation()}>          
+            <div className={classes.modalHeader}>
+              <h3>{`${isEditMode ? '編輯' : '新增'}訂單`}</h3>
+              <div className={classes.divier} />
+              <span type="button" className={classes.closeBtn} onClick={onClickClose}>
+                &times;
+              </span>
+            </div>
+            <div className={classes.modalBody}>
+              <div className={classes.inRowClass}>
+                <h5 className={classes.h5}>品項:</h5>
+                <input type='text' value={name} onChange={onChangeName} className={classes.textInput}/>
+              </div>
+              <div className={classes.inRowClass}>
+                <h5 className={classes.h5}>金額:</h5>
+                <input required type='number' step="5" min="0" value={price} onChange={onChangePrice} className={classes.textInput}/>
+              </div>
+              <div></div>
+              <div className={classes.inRowClass}>
+                <h5 className={classes.h5}>備註:</h5>
+                <textarea value={notes} onChange={onChangeNotes} className={classes.textArea}/>
+              </div>
+              <div></div>
+            </div>
+            <div className={classes.modalFooter}>
+              <div className={classes.btn} onClick={onClickCancel}>取消</div>
+              <div className={classes.btn} onClick={onClickConfirm}>確認</div>
+            </div>
           </div>
         </div>
       }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,9 +3,14 @@ import OrderModal from '../components/OrderModal';
 
 const samples = [
   {
-    name: 'n1', // string
+    name: '奶茶', // string
     price: 40, // number
-    notes: '' // optional string
+    notes: '大杯\n無糖\n去冰\n' // optional string
+  },
+  {
+    name: '紅茶', // string
+    price: 30, // number
+    notes: '中杯 無糖 常溫' // optional string
   }
 ]
 
@@ -13,32 +18,39 @@ const samples = [
 
 const HomePage = (props) => {
   const [orders, setOrders] = useState(samples);
-  const onDelete = (key) => () => {};
-  const onEditOrder = (key) => (data) => {
-    orders[key] = data;
+  const onDelete = (orderIndex) => () => {
+    orders.splice(orderIndex, 1);
+    setOrders([...orders]);
+  };
+  const onEditOrder = (orderIndex) => (data) => {
+    orders[orderIndex] = data;
     setOrders([...orders]);
   }
   return (
     <div>
       <table>
-        <tr>
-          <th>品項</th>
-          <th>價錢</th>
-          <th>備註</th>
-          <th>編輯</th>
-          <th>刪除</th>
-        </tr>
-        {orders.map(({ name, price, notes}, index) => {
+        <thead>
+          <tr>
+            <th>品項</th>
+            <th>價錢</th>
+            <th>備註</th>
+            <th>編輯</th>
+            <th>刪除</th>
+          </tr>
+        </thead>
+        <tbody>
+        {orders.map((order, index) => {
           return (
-            <tr>
-              <td>{name}</td>
-              <td>{price}</td>
-              <td>{notes}</td>
-              <td><OrderModal isEditMode onEditOrder={onEditOrder(index)}/></td>
+            <tr key={index}>
+              <td>{order.name}</td>
+              <td>{order.price}</td>
+              <td>{order.notes || '無'}</td>
+              <td><OrderModal isEditMode data={order} onEditOrder={onEditOrder(index)}/></td>
               <td onClick={onDelete(index)}>delete Logo</td>
             </tr>
           )
         })}
+        </tbody>
       </table>
     </div>
   )
