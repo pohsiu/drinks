17a049d98ba5765344a628c5a771ed2170b8335a
Louis Hsiao <louis.hsiao@d8ai.com>
2020-01-07 22:52:19 +0800

    Updates

---
+++ precache-manifest.26090d2487b72634d281fbeb47f3e5eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8123d093454a743b199c6588dc167218",
+    "url": "/drinks/index.html"
+  },
+  {
+    "revision": "029ce147841df8416db8",
+    "url": "/drinks/static/css/main.e0a7d051.chunk.css"
+  },
+  {
+    "revision": "97946f18955c5a5cdb54",
+    "url": "/drinks/static/js/2.d41525c1.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/drinks/static/js/2.d41525c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "029ce147841df8416db8",
+    "url": "/drinks/static/js/main.6275cc28.chunk.js"
+  },
+  {
+    "revision": "476f5e129e314d3eeb83",
+    "url": "/drinks/static/js/runtime-main.3e3e7c4b.js"
+  }
+]);
