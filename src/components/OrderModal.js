2b23d6e52794b8a1224daf5841beb42de3513e82
Louis Hsiao <louis.hsiao@d8ai.com>
2020-01-07 20:57:22 +0800

    feat: better ui

--- src/components/OrderModal.js
+++ src/components/OrderModal.js
@@ -17,21 +17,33 @@ const useStyles = createUseStyles({
   modalContent: {
     backgroundColor: '#fefefe',
     margin: 'auto',
-    padding: '25px 40px',
     border: '1px solid #888',
     width: '40%',
+    position: 'relative',
+    borderRadius: 12,
+    paddingBottom: 20,
   },
   modalHeader: {
     display: 'flex',
+    backgroundColor: 'rgb(35, 73, 118)',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    padding: 20,
+    paddingLeft: 32,
+    color: '#fff',
   },
   modalBody: {
+    paddingTop: 16,
+    paddingLeft: 32,
+    paddingRight: 32,
     display: 'flex',
     flexDirection: 'column',
   },
   modalFooter: {
     marginTop: 24,
+    paddingRight: 32,
     display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
   inRowClass: {
     display: 'flex',
@@ -40,12 +52,12 @@ const useStyles = createUseStyles({
     alignItems: 'flex-start',
   },
   textInput: {
-    width: '25vw',
+    flexGrow: 1,
     marginLeft: 16,
   },
   textArea: {
     marginLeft: 16,
-    width: '25vw',
+    flexGrow: 1,
     height: 60,
     resize: 'none',
   },
@@ -53,31 +65,54 @@ const useStyles = createUseStyles({
     flexGrow: 1,
   },
   btn: {
+    width: 20,
+    height: 20,
+    padding: '4px 4px',
+    textAlign: 'center',
+    
+    borderRadius: 6,
+    '&:hover': {
+      cursor: 'pointer',
+      transform: 'translateY(-1px)',
+    },
+    '&:active': {
+      transform: 'translateY(3px)',
+    },
+    margin: 'auto',
+  },
+  cancelClass: {
+    margin: 4,
     width: 40,
     height: 28,
     lineHeight: '28px',
-    padding: '4px 4px',
-    cursor: 'pointer',
-    border: '#000 1px solid',
-    textAlign: 'center',
-    margin: 4,
-  },
-  closeBtn: {
-    color: '#aaaaaa',
-    float: 'right',
-    fontSize: 28,
-    fontWeight: 'bold',
+    color: '#fff',
+    backgroundColor: 'rgb(203, 68, 75)',
     '&:hover': {
-      color: '#000',
-      textDecoration: 'none',
-      cursor: 'pointer',
-    }
+      backgroundColor: 'rgb(203, 68, 75, 0.8)'
+    },
+  },
+  confirmClass: {
+    margin: 4,
+    width: 40,
+    height: 28,
+    lineHeight: '28px',
+    color: '#fff',
+    marginRight: 0,
+    backgroundColor: 'rgb(83, 164, 81)',
+    '&:hover': {
+      backgroundColor: 'rgb(83, 164, 81, 0.8)'
+    },
+  },
+  modalCloseClass: {
+    position: 'absolute',
+    top: 4,
+    right: 8,
   }
 })
 
 
 const OrderModal = (props) => {
-  const { isEditMode, data, onEditOrder } = props;
+  const { isEditMode, data = {}, onEditOrder, onAddOrder } = props;
   const classes = useStyles();
   const [name, setName] = useState(data.name);
   const [price, setPrice] = useState(data.price);
@@ -87,11 +122,20 @@ const OrderModal = (props) => {
   const onClickOpen = () => setModelVisible(true);
   const onClickCancel = onClickClose;
   const onClickConfirm = () => {
-    onEditOrder({
-      name,
-      price,
-      notes,
-    })
+    if (isEditMode) {
+      onEditOrder({
+        name,
+        price,
+        notes,
+      })
+    } else {
+      onAddOrder({
+        name,
+        price,
+        notes,
+      })
+    }
+    
     onClickClose();
   }
   const onChangeName = (e) => setName(e.target.value);
@@ -99,17 +143,16 @@ const OrderModal = (props) => {
   const onChangeNotes = (e) => setNotes(e.target.value);
   return (
     <div>
-      <button onClick={onClickOpen}>{isEditMode ? 'Edit' : 'Add'}</button>
-
-      {modelVisible && isEditMode &&
+      <div className={classes.btn} onClick={onClickOpen}>{isEditMode ? <i className="fa fa-edit" /> : <i className="fa fa-plus" />}</div>
+      {modelVisible &&
         <div className={classes.modalStyle} onClick={onClickClose}>
-          <div className={classes.modalContent} onClick={(e) => e.stopPropagation()}>          
+          <div className={classes.modalContent} onClick={(e) => e.stopPropagation()}>
+            <div className={`${classes.btn} ${classes.modalCloseClass}`} onClick={onClickClose}>
+              <i className="fa fa-close" />
+            </div>        
             <div className={classes.modalHeader}>
               <h3>{`${isEditMode ? '編輯' : '新增'}訂單`}</h3>
               <div className={classes.divier} />
-              <span type="button" className={classes.closeBtn} onClick={onClickClose}>
-                &times;
-              </span>
             </div>
             <div className={classes.modalBody}>
               <div className={classes.inRowClass}>
@@ -120,16 +163,14 @@ const OrderModal = (props) => {
                 <h5 className={classes.h5}>金額:</h5>
                 <input required type='number' step="5" min="0" value={price} onChange={onChangePrice} className={classes.textInput}/>
               </div>
-              <div></div>
               <div className={classes.inRowClass}>
                 <h5 className={classes.h5}>備註:</h5>
                 <textarea value={notes} onChange={onChangeNotes} className={classes.textArea}/>
               </div>
-              <div></div>
             </div>
             <div className={classes.modalFooter}>
-              <div className={classes.btn} onClick={onClickCancel}>取消</div>
-              <div className={classes.btn} onClick={onClickConfirm}>確認</div>
+              <div className={`${classes.btn} ${classes.cancelClass}`} onClick={onClickCancel}>取消</div>
+              <div className={`${classes.btn} ${classes.confirmClass}`} onClick={onClickConfirm}>確認</div>
             </div>
           </div>
         </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { createUseStyles } from 'react-jss';
 import OrderModal from '../components/OrderModal';
 
 const samples = [
@@ -10,14 +11,86 @@ const samples = [
   {
     name: '紅茶', // string
     price: 30, // number
-    notes: '中杯 無糖 常溫' // optional string
+    notes: '中杯\n無糖\n常溫' // optional string
+  },
+  {
+    name: '珍奶', // string
+    price: 50, // number
+    notes: '中杯\n無糖\n常溫' // optional string
   }
 ]
 
+const useStyles = createUseStyles({
+  root: {
+    width: '60vw',
+    margin: 'auto',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  header: {
+    padding: 24,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btn: {
+    width: 20,
+    height: 20,
+    padding: '4px 4px',
+    textAlign: 'center',
+    margin: 4,
+    borderRadius: 6,
+    '&:hover': {
+      cursor: 'pointer',
+      transform: 'translateY(-1px)',
+    },
+    '&:active': {
+      transform: 'translateY(3px)',
+    },
+    margin: 'auto',
+  },
+  theader: {
+    backgroundColor: 'rgb(35, 73, 118)',
+    color: '#fff',
+    height: 40,
+    borderBottom: '1px solid white',
+  },
+  table: {
+    borderCollapse: 'collapse',
+  },
+  tr: {
+    borderBottom: '2px solid white',
+    paddingTop: 12,
+    paddingBottom: 12,
+  },
+  trOdd: {
+    backgroundColor: 'grey',
+    color: 'white',
+    borderBottom: '2px solid black',
+  },
+  td: {
+    padding: 2,
+    wordWrap: 'break-word',
+    whiteSpace: 'pre-line',
+    textAlign: 'center',
+    minWidth: '10vw',
+  },
+  divider: {
+    backgroundColor: 'rgb(35, 73, 118)',
+    height: 2,
+    marginBottom: 20,
+  }
+})
 
+const StyleTd = (props) => {
+  const classes = useStyles();
+  const { children, ...others } = props;
+  return <td className={classes.td} {...others}>{children}</td>
+}
 
 const HomePage = (props) => {
   const [orders, setOrders] = useState(samples);
+  const classes = useStyles();
   const onDelete = (orderIndex) => () => {
     orders.splice(orderIndex, 1);
     setOrders([...orders]);
@@ -26,11 +99,21 @@ const HomePage = (props) => {
     orders[orderIndex] = data;
     setOrders([...orders]);
   }
+  const onAddOrder = (data) => {
+    orders.push(data);
+    setOrders([...orders]);
+  }
   return (
-    <div>
-      <table>
+    <div className={classes.root}>
+      <div className={classes.header}>
+        <h4>訂飲料</h4>
+        <OrderModal onAddOrder={onAddOrder} />
+      </div>
+      <div className={classes.divider}/>
+      {orders.length === 0 && <h4>尚無任何訂單，請新增訂單</h4>}
+      {orders.length > 0 && <table className={classes.table}>
         <thead>
-          <tr>
+          <tr className={classes.theader}>
             <th>品項</th>
             <th>價錢</th>
             <th>備註</th>
@@ -41,17 +124,17 @@ const HomePage = (props) => {
         <tbody>
         {orders.map((order, index) => {
           return (
-            <tr key={index}>
-              <td>{order.name}</td>
-              <td>{order.price}</td>
-              <td>{order.notes || '無'}</td>
-              <td><OrderModal isEditMode data={order} onEditOrder={onEditOrder(index)}/></td>
-              <td onClick={onDelete(index)}>delete Logo</td>
+            <tr className={index % 2 === 0 ? classes.trOdd : classes.tr} key={index}>
+              <StyleTd>{order.name}</StyleTd>
+              <StyleTd>{order.price}</StyleTd>
+              <StyleTd>{order.notes || '無'}</StyleTd>
+              <StyleTd><OrderModal isEditMode data={order} onEditOrder={onEditOrder(index)}/></StyleTd>
+              <StyleTd><div className={classes.btn} onClick={onDelete(index)}><i className="fa fa-trash" /></div></StyleTd>
             </tr>
           )
         })}
         </tbody>
-      </table>
+      </table>}
     </div>
   )
 }
